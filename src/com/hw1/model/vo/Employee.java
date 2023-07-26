0aefb5a42ab4a6bbd6c00ed223d46cf36ed5dbf8
eee595398 <eee595398@gmail.com>
2023-07-26 22:46:43 +0900

    1

---
+++ src/com/hw1/model/vo/Employee.java
@@ -0,0 +1,19 @@
+package com.hw1.model.vo;
+
+public class Employee {
+
+	private int empNo;
+	private String empName;
+	private String dept;
+	private String job;
+	private int age;
+	private char gender;
+	private int salary;
+	private double bonusPoint;
+	private String phone;
+	private String address;
+	
+	public void Employee() {}
+	
+	
+}
---
+++ src/com/hw1/run/Run.java
@@ -0,0 +1,9 @@
+package com.hw1.run;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
